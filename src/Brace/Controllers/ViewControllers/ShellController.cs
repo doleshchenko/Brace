7f3b766852e1afad4c7e11d93ab730dc14525115
Dmytro Oleshchenko <dima.oleshchenko@outlook.com>
2017-03-18 23:59:56 +0100

    Remove  minification for development env.

--- src/Brace/Controllers/ViewControllers/ShellController.cs
+++ src/Brace/Controllers/ViewControllers/ShellController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Brace.Controllers
+namespace Brace.Controllers.ViewControllers
 {
     public class ShellController : Controller
     {
--- src/Brace/wwwroot/js/shell.js
+++ src/Brace/wwwroot/js/shell.js
@@ -1,1 +1,17 @@
-﻿
+﻿(function (ko) {
+    "use strict";
+    var ShellViewModel = function() {
+        this.commandLine = ko.observable("please enter the command");
+        this.onkeypress = function(data, e) {
+            var event = e || window.event;
+            var charCode = event.which || event.keyCode;
+
+            if (charCode === 13) {
+                console.log("user pressed enter key");
+            }
+            return true;
+        }
+    };
+
+    ko.applyBindings(new ShellViewModel(), document.getElementById("shell"));
+}(ko));
