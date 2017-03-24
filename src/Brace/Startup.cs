415ea74ef96cd10879f4289c4d075f409aac9eac
Dmytro Oleshchenko <dima.oleshchenko@outlook.com>
2017-03-24 07:59:14 +0100

    Cosmetic changes: reordering

--- src/Brace/Startup.cs
+++ src/Brace/Startup.cs
@@ -46,10 +46,9 @@ namespace Brace
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //add NLog to ASP.NET Core
-            loggerFactory.AddNLog();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+            loggerFactory.AddNLog();
 
             app.UseMvc();
             app.UseStaticFiles();
